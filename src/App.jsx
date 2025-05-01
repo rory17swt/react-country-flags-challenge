e9c5ce55e506f9b4882cd5cc018706c8fa587972
Rory Swietlicki <rory@swietlicki.eu>
2025-05-01 16:11:32 +0100

    Added name and flag

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // Var
+  const [countries, setCountries] = useState([])
+
+  // Events
+  useEffect(() => {
+    async function getCountries() {
+      try {
+        // 1. Consume API
+        const { data } = await axios.get('https://restcountries.com/v3.1/all')
+
+        // 2. Set the data to the state varible
+        setCountries(data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getCountries()
+  }, [])
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {countries.length > 0 &&
+    countries.map(flag => {
+      return (
+        <div key={flag.name.common}>
+          <h2>{flag.name.common}</h2>
+          <h3>{flag.name.official}</h3>
+          <img src={flag.flags.png}></img>
+        </div>
+      )
+    })}
     </>
   )
+
 }
 
-export default App
+
